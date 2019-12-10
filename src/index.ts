c0009d8fcfc95de2c839d503662d29b954c192f2
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-12-10 12:03:42 +0200

    add empty stack and tests around it

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,12 @@
 //
 import * as cdk from '@aws-cdk/core'
 
-const app = new cdk.App()
+export class Stack extends cdk.Stack {
+  constructor(app: cdk.App, id: string, props: cdk.StackProps) {
+    super(app, id, props)
+  }
+}
 
+const app = new cdk.App()
+new Stack(app, 'privx-on-aws', {})
 app.synth()
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,7 @@
-test('empty test', () => {
+import * as cdk from '@aws-cdk/core'
+import { Stack } from '../src/index'
 
+test('stack spawns required resources', () => {
+  const app = new cdk.App()
+  new Stack(app, 'test-stack', {})
 })
